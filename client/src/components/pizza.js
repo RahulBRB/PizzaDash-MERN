7cc1d2fabc8f77f290747a01137aacef12151605
Rahul Baskey <rahulblue17@gmail.com>
2023-09-16 14:08:39 +0530

    set up pizza

---
+++ client/src/components/pizza.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Pizza({pizza}) {
+  return (
+    <div>pizza</div>
+  )
+}
--- client/src/screens/Homescreen.js
+++ client/src/screens/Homescreen.js
@@ -1,10 +1,21 @@
 import React from 'react'
+import Pizza from '../components/pizza'
 import pizzas from '../pizzasdata'
 export default function Homescreen() {
   return (
     <div>
+        <div className='row'>
+    {pizzas.map(pizza=>{
+        return <div className='col-md-4'>
+                 <div>
+                <Pizza pizza={pizza}/>
+                </div>
 
-    <h1>{pizzas.length}</h1>
+            </div>
+            
+            })}
+
+        </div>
     </div>
   )
 }
