4a95cf6ec4c7f9fc00732bcdb16f214789ae9a9f
Rahul Baskey <rahulblue17@gmail.com>
2023-09-16 13:24:58 +0530

    navbar

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <div>
+        <nav className="navbar navbar-expand-sm bg-primary navbar-dark">
+  <div className="container-fluid">
+    <ul className="navbar-nav">
+      <li className="nav-item">
+        <a className="nav-link active" href="#">Active</a>
+      </li>
+     <li classNameName="nav-item">
+        <a className="nav-link" href="#">Link</a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="#">Link</a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link disabled" href="#">Disabled</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+    </div>
+    
+  )
+}
