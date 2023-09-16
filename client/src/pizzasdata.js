971360a2c17ac6d70ad40fa68943b5b85bd0ca13
Rahul Baskey <rahulblue17@gmail.com>
2023-09-16 14:01:17 +0530

    added pizzas info

---
+++ client/src/pizzasdata.js
@@ -0,0 +1,70 @@
+const pizzas = [
+    {
+      "name": "PEPPER BARBECUE CHICKEN",
+      "varients": ["small", "medium", "large"],
+      "prices": {
+        "small": 200,
+        "medium": 350,
+        "large": 400
+      },
+      "category": "nonveg",
+      "image": "#",
+      "description": "PEPPER BARBECUE"
+    }, {
+        "name": "MARGHERITA PIZZA",
+        "varients": ["small", "medium", "large"],
+        "prices": {
+          "small": 180,
+          "medium": 300,
+          "large": 400
+        },
+        "category": "veg",
+        "image": "#",
+        "description": "Classic Margherita pizza with tomato, mozzarella, and basil."
+      }, {
+        "name": "PEPPERONI PIZZA",
+        "varients": ["small", "medium", "large"],
+        "prices": {
+          "small": 220,
+          "medium": 370,
+          "large": 450
+        },
+        "category": "nonveg",
+        "image": "#",
+        "description": "Delicious pepperoni pizza with spicy pepperoni and cheese."
+      },{
+        "name": "VEGGIE SUPREME PIZZA",
+        "varients": ["small", "medium", "large"],
+        "prices": {
+          "small": 200,
+          "medium": 340,
+          "large": 420
+        },
+        "category": "veg",
+        "image": "#",
+        "description": "Loaded with assorted veggies, olives, and cheese."
+      },{
+        "name": "BBQ CHICKEN PIZZA",
+        "varients": ["small", "medium", "large"],
+        "prices": {
+          "small": 240,
+          "medium": 390,
+          "large": 480
+        },
+        "category": "nonveg",
+        "image": "#",
+        "description": "Savory BBQ chicken pizza with barbecue sauce and cheese."
+      },{
+        "name": "HAWAIIAN PIZZA",
+        "varients": ["small", "medium", "large"],
+        "prices": {
+          "small": 210,
+          "medium": 360,
+          "large": 440
+        },
+        "category": "nonveg",
+        "image": "#",
+        "description": "Classic Hawaiian pizza with ham, pineapple, and cheese."
+      }   
+  ]
+  export default pizzas;
