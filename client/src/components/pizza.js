025d9ff63096e6d5c1bba7b721d5e555bbb9021c
Rahul Baskey <rahulblue17@gmail.com>
2023-09-16 14:30:26 +0530

    variants prices

--- client/src/components/pizza.js
+++ client/src/components/pizza.js
@@ -5,6 +5,19 @@ export default function Pizza({pizza}) {
     <div>
         <h1>{pizza.name}</h1>
         <img src={pizza.image} className='img-fluid' style={{height:"200px", width:"auto"}} />
+
+        <div className='flex-container'>
+            <div className='w-100'>
+                <p>Varients</p>
+            </div>
+
+
+            <div className='w-100'>
+                <p>Prices</p>
+            </div>
+
+            
+            </div>'
     </div>
   )
 }
