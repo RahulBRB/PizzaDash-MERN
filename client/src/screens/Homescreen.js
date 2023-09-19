0c91ae506b7a7ae2917db034b03125ce8392017f
Rahul Baskey <rahulblue17@gmail.com>
2023-09-19 13:38:11 +0530

    homescreen

--- client/src/screens/Homescreen.js
+++ client/src/screens/Homescreen.js
@@ -1,12 +1,19 @@
-import React from 'react'
+import React, {useState, useEffect } from 'react'
+import { useDispatch,useSelector } from 'react-redux'
+import {getAllPizzas} from '../actions/pizzaActions'
 import Pizza from '../components/pizza'
 import pizzas from '../pizzasdata'
 export default function Homescreen() {
+
+    const dispatch = useDispatch() 
+    useEffect(() =>{
+      dispatch(getAllPizzas())
+    }, [])
   return (
     <div>
         <div className='row'>
-    {pizzas.map(pizza=>{
-        return <div className='col-md-4'>
+          {pizzas.map(pizza=>{
+            return <div className='col-md-4'>
                  <div>
                 <Pizza pizza={pizza}/>
                 </div>
