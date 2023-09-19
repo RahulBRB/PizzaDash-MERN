c33729abb0d42058544d64243eca54caab8ea210
Rahul Baskey <rahulblue17@gmail.com>
2023-09-19 13:10:44 +0530

    importing from redux and redux thunk

---
+++ client/src/store.js
@@ -0,0 +1,5 @@
+import combinedReducers from 'redux'
+import {createStore, applyMiddleware} from 'redux'
+import thunk from 'redux-thunk'
+import {composeWithDevTools} from 'redux-devtools-extension'
+
