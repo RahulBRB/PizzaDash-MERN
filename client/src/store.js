a8fc4ec621df77577b05ac090a6d73fcfd89e875
Rahul Baskey <rahulblue17@gmail.com>
2023-09-19 13:13:43 +0530

    creating store

--- client/src/store.js
+++ client/src/store.js
@@ -1,5 +1,14 @@
 import combinedReducers from 'redux'
 import {createStore, applyMiddleware} from 'redux'
+import { getAllPizzasReducer } from './reducers/pizzaReducers'
 import thunk from 'redux-thunk'
 import {composeWithDevTools} from 'redux-devtools-extension'
+
+const finalReducer = combineReducers({
+    getAllPizzasReducer: getAllPizzasReducer
+})
+
+const initialState = {}
+const composeEnhancers = composeWithDevTools({})
+const store = createStore(finalReducer, initialState, composeEnhancers(applyMiddleware(thunk)))
 
